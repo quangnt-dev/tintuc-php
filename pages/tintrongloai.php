15f1859934803e5b3ddb10f60ab5e26d7e6a24bd
quangnt-dev <quangnt.dev@gmail.com>
2021-03-17 17:19:33 +0700

    original project config add pages chitiettin and tintrongloai

---
+++ pages/tintrongloai.php
@@ -0,0 +1,49 @@
+<div>
+	Trang chủ >>
+</div>
+
+
+<div class="box-cat">
+	<div class="cat1">
+
+		<div class="clear"></div>
+		<div class="cat-content">
+			<div class="col0 col1">
+				<div class="news">
+					<h3 class="title"><a href="chitiet/-.html"></a></h3>
+					<img class="images_news" src="upload/tintuc/" align="left" />
+					<div class="des"></div>
+					<div class="clear"></div>
+
+				</div>
+			</div>
+
+		</div>
+	</div>
+</div>
+<div class="clear"></div>
+
+
+<!-- box cat-->
+<hr />
+<style>
+	#phantrang {
+		text-align: center;
+	}
+
+	#phantrang a {
+		background-color: #000;
+		color: #FF0;
+		padding: 5px;
+		margin-right: 3px
+	}
+
+	#phantrang a:hover {
+		background-color: #09F;
+	}
+</style>
+<div id="phantrang">
+
+	<a href="index.php?p=tintrongloai&idLT=&trang="></a>
+
+</div>
